c759cbce6a593e118d3e12a759bb10d15b1a8f5e
matan.benyamin <bardugo2!>
2021-09-06 09:09:44 +0300

    added wave in case forecast is close

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import numpy as np
 
 soup = wf.get_windfinder_soup()
 
-req_date= datetime.strptime('Sep 13 ' + str(date.today().year), '%b %d %Y')
+req_date= datetime.strptime('Sep 7 ' + str(date.today().year), '%b %d %Y')
 req_date= 'last'
 c, given_date = wf.get_date_div(soup, req_date)
 
 d6 = wf.get_wind_at_time(c, 6)
-d9 = c[3].find_all('span',{"class": "units-ws"})[0].get_text()
+d9 = wf.get_wind_at_time(c, 9)
 d7 = 0.333*float(d9)+0.6666*float(d6)
 print('wind in 7:00: ' + str(np.round(d7,0)) + ' Kts' )
 
+w6 = wf.get_wave_at_time(c, 6)
 
 
--- scraper/windfinder.py
+++ scraper/windfinder.py
@@ -33,12 +33,22 @@ def get_date_div(soup, req_date):
     given_date = given_date[given_date.find('\n',1)+10:given_date.find('\n',2)].strip()
     print(given_date)
     b = all_divs[date_ind].find_parent()
-    return  b.find_all("div", {"class": "speed"}), given_date
+    return  b, given_date
 
 def get_wind_at_time(date_div, time):
+    date_div_wind = date_div.find_all("div", {"class": "speed"})
     hours = [24, 3, 6, 9 ,12, 15, 18, 21]
     ind = hours.index(time)
     if ind == -1:
         return -1
 
-    return date_div[ind].find_all('span',{"class": "units-ws"})[0].get_text()
+    return date_div_wind[ind].find_all('span',{"class": "units-ws"})[0].get_text()
+
+def get_wave_at_time(date_div, time):
+    date_div_wave = date_div.find_all("div", {"class": "data-waveheight"})
+    hours = [24, 3, 6, 9 ,12, 15, 18, 21]
+    ind = hours.index(time)
+    if ind == -1 or len(date_div_wave)==0:
+        return -1
+
+    return date_div_wave[ind].find_all('span',{"class": "units-wh"})[0].get_text()
