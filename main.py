f3d8da25200d44c342ac7f5c862eb63fa293431d
matan.benyamin <bardugo2!>
2021-09-06 08:45:41 +0300

    edited main

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from scraper import windfinder
+from scraper import windfinder as wf
+from datetime import datetime, date
+import numpy as np
 
-soup = get_windfinder_soup()
+
+soup = wf.get_windfinder_soup()
 
 req_date= datetime.strptime('Sep 13 ' + str(date.today().year), '%b %d %Y')
 req_date= 'last'
-c, given_date = get_date_div(soup, req_date)
+c, given_date = wf.get_date_div(soup, req_date)
 
-d6 = get_wind_at_time(c, 6)
+d6 = wf.get_wind_at_time(c, 6)
 d9 = c[3].find_all('span',{"class": "units-ws"})[0].get_text()
 d7 = 0.333*float(d9)+0.6666*float(d6)
 print('wind in 7:00: ' + str(np.round(d7,0)) + ' Kts' )
